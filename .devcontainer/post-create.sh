35417adbc6b350072252d26ecb4f18aaa57c1b25
Taras Romashko <taras.romashko@jet.dev>
2024-12-22 20:30:25 +0200

    fix: remove redundant stuff

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
 
-# Check if the launch.json exists
-# if ! [ -f .vscode/launch.json ]; then
-#   printf "\nCopy vscode launch.json file.. \n"
-#   mkdir -p .vscode
-#   cp /code/.devcontainer/launch.json /code/.vscode/launch.json
-# fi
-
 # exec symfony server:start --no-tls --port=8000 --allow-http --no-interaction > /dev/null 2>&1 &
